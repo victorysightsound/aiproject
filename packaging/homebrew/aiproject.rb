a5d0473eca6796da86c78cf6fb2c16c79893f5c8
victorysightsound <john@victorysightsound.com>
2026-01-21 23:02:37 -0600

    Update Homebrew formula with release SHA256 hashes

--- packaging/homebrew/aiproject.rb
+++ packaging/homebrew/aiproject.rb
@@ -7,22 +7,22 @@ class Aiproject < Formula
   on_macos do
     on_arm do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_ARM64"
+      sha256 "5bdb51e53aee80d824758e5715d94da64e8814894e9f173aeec14b7a7d5aac6d"
     end
     on_intel do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_X64"
+      sha256 "395dc03ceef056101a65eb254e373c27a5de67e2db3b09aabb4bd075efefe346"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "3247b8c86f228ef208bbcc89c51833751c83de9df18127431dc65c4617061d19"
     end
     on_intel do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X64"
+      sha256 "a383479a975c5eb96b3573955b64612577400b15a92d48281a0b4555f2875c3a"
     end
   end
 
