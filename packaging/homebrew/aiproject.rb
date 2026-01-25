29a83e32e29d25011c57ab50d372e2451e43840a
victorysightsound <john@victorysightsound.com>
2026-01-24 18:41:17 -0600

    Update Homebrew formula for v1.1.0

--- packaging/homebrew/aiproject.rb
+++ packaging/homebrew/aiproject.rb
@@ -1,28 +1,28 @@
 class Aiproject < Formula
   desc "Project tracking and context management for AI-assisted development"
   homepage "https://github.com/victorysightsound/aiproject"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-aarch64-apple-darwin.tar.gz"
-      sha256 "5bdb51e53aee80d824758e5715d94da64e8814894e9f173aeec14b7a7d5aac6d"
+      sha256 "e9097bfb6fa72cdc1df7910bb0b6d094cd60971ebb6aece40f6dab17873a1c5f"
     end
     on_intel do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-x86_64-apple-darwin.tar.gz"
-      sha256 "395dc03ceef056101a65eb254e373c27a5de67e2db3b09aabb4bd075efefe346"
+      sha256 "2b2c6dfb4be43498bdeaf0ac97b9d7996949d01cf78638768379bbf39791ba01"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3247b8c86f228ef208bbcc89c51833751c83de9df18127431dc65c4617061d19"
+      sha256 "720ccb122ac9d7ef96d2a329158d94f0d37cf6837697ef1d38a76d892fdc7160"
     end
     on_intel do
       url "https://github.com/victorysightsound/aiproject/releases/download/v#{version}/proj-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "a383479a975c5eb96b3573955b64612577400b15a92d48281a0b4555f2875c3a"
+      sha256 "9cba13650e0be9c47fd5eef2e13e535b7a7f5df44845f823f3cb5096e031fa71"
     end
   end
 
